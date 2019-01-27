dd3bafbadc85e679a8c7dbc51fc0d8f0d7a02c8e
preeti kumari <pkumari@itechmission.org>
2019-01-27 17:45:53 +0530

    blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component , ngOnit } from '@angular/core';
+import { RouterModule, Routes , Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements ngOnit{
   title = 'app';
+
+  constructor(public router: Router){}
 }
--- src/app/banner-slider/banner-slider.component.ts
+++ src/app/banner-slider/banner-slider.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient, HttpParams , HttpResponseBase, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-banner-slider',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerSliderComponent implements OnInit {
 
-  constructor() { }
+   public data_content_slider:any;
+  constructor( private http: HttpClient ) { }
+   
+  getBlog_slide() {
+      this.http.get('http://localhost/blog/api/get_post.php',{responseType:"json"})
+      .subscribe(
+        response => {
+            this.data_content_slider = response;
+            console.log("data :"+response);
+            var sample=JSON.stringify(response);
+       });
+  }
 
   ngOnInit() {
+      this.getBlog_slide();
   }
 
 }
